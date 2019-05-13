601e91aa79edbf2aec4274f462d5fa5d35040066
ernesto <ernestozarza91@yahoo.com>
2019-05-13 10:56:59 +0200

    Modifying the url's configuration

--- airport_data_collector/urls.py
+++ airport_data_collector/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url('airports/list/$', ListAirportView.as_view(), name='airports-all'),
     url('airports/$', ListAirportView.as_view(), name='airports-all'),
     url('^airports/name/(?P<airport_name>[\w-]+)/$', AirportNameListView.as_view(), name="airports-name-list"),
-    url('^airports/iata/(?P<airports_iata>[\w-]+)/$', AirportIATADetailView.as_view(), name="airports-iata-detail"),
+    url('^airports/iata/(?P<airport_iata>[\w-]+)/$', AirportIATADetailView.as_view(), name="airports-iata-detail"),
 
 ]
